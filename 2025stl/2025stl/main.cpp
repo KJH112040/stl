294dc5410ffdda6d3c33e50cbe2ab86dc8321141
KJH04_ <10wjdgp04@gmail.com>
2025-03-17 17:35:25 +0900

    .

--- 2025stl/2025stl/main.cpp
+++ 2025stl/2025stl/main.cpp
@@ -1,70 +1,41 @@
 //------------------------------------------------------------------------------------------------
-//	2025 STL 화56목78	3월 6일 목요일				(1주 2일)
+//	2025 STL 화56목78	3월 13일 목요일				(2주 2일)
 //------------------------------------------------------------------------------------------------
-//	컴파일 환경 확인 / 한 학기 강의를 저장할 save 만들기
-//	과제 - save를 save.cpp로 분리하라.
-//------------------------------------------------------------------------------------------------
-//	VS 버젼 - 17.13 이상
-//	Release / x64, C++ 언어 표준 - std::C++latest, SDL 검사 - 아니요
+//	많은 수의 자료를 처리하기	-	파일에 저장하고 읽어 오기
 //------------------------------------------------------------------------------------------------
 #include <iostream>
-#include <string>
-#include <fstream>
-#include <filesystem>
-#include<vector>
+#include<fstream>
 #include<algorithm>
-#include<chrono>
+#include<array>
+#include<print>
+#include "save.h"
+using namespace std;
 
-//	[문제] save 함수를 제대로 코딩하라.
-void save(std::string_view);
-//	C++ 사용 x	
-//	1. T[];	--->	array<T,N>
-//	2. char*	---->	srting
-// 	3. T*	--->	unique str...ㅠ 못 적음
+//	[문제] 파일 "int 1000개.txt"에는 int 값 1000개가 text로 기록되어 있다.
+//	이 데이터를 메모리에 저장하라.
+//	메모리에 있는 데이터를 화면에 출력하라.		(5분)
 
 //---------
 int main() 
 //---------
 {
-	std::cout << "2025 STL" << std::endl;
-	
-	save("main.cpp");
-}
+	ifstream in{ "int 1000개.txt" };
+	if (not in)
+		return 20250313;
 
-//------------------------------
-void save(std::string_view fileName)
-//------------------------------
-{
-	//	fileName를 읽기모드로 연다	ifstream
-	std::ifstream in{ fileName.data() };	//	RAII
-	if (not in) {
-		std::cout << fileName << "을 열 수 없습니다." << std::endl;
-		exit(20250306);
+	array<int, 1000> arr;
+	int num;/*, cnt{0};
+	while (in >> num) {
+		in >> arr[cnt];
+		++cnt;
+	}*/
+	for (int i = 0; i < 1000; ++i) {
+		in >> num;
+		arr[i] = num;
 	}
 
-	//	쓰기 모드로 저장할 파일을 연다	ofstream
-	std::string nameToSave{ "2025 1 STL 화56목78 강의저장.txt" };
-	std::ofstream out{ nameToSave,std::ios::app };
+	for (int num : arr)
+		print("{:8}", num);
 
-	//	저장했다고 화면 출력한다.
-	auto size = std::filesystem::file_size(fileName);
-	std::cout <<"\"" << nameToSave << "에 " << fileName << "을 덧붙였습니다. - " 
-		<< size << " bytes\"" << std::endl;
-
-	//	저장한 시간을 파일에 기록하자
-	auto now = std::chrono::system_clock::now();			//	time_point를 얻는다 epoch:1970년 1월 1일 자정
-	auto utc = std::chrono::system_clock::to_time_t(now);	//	UTC 시간으로 변경
-	auto lt = std::localtime(&utc);							//	지역달력 시간으로
-	auto old = out.imbue(std::locale("ko_KR"));
-
-	out << '\n' << '\n';
-	out << "=================================================" << '\n';
-	out << "저장한 시간: " << std::put_time(lt, "%c %A") << '\n';
-	out << "=================================================" << '\n';
-	out << '\n';
-
-	out.imbue(old);		//	원래 locale을 복구
-	
-	//	읽을 파일에 있는 모든 내용을 읽어 쓸 파일에 덧붙여 쓴다
-	std::copy(std::istreambuf_iterator{ in }, {}, std::ostreambuf_iterator{ out });
+	save("main.cpp");
 }
