d75f7a14dc24fb4cf00f2664f68ded2e22729127
KJH04_ <10wjdgp04@gmail.com>
2025-03-20 17:11:48 +0900

    .

--- 2025stl/2025stl/main.cpp
+++ 2025stl/2025stl/main.cpp
@@ -1,28 +1,48 @@
 //------------------------------------------------------------------------------------------------
-//	2025 STL 화56목78	3월 18일 화요일				(3주 1일)
+//	2025 STL 화56목78	3월 20일 목요일				(3주 2일)
 //------------------------------------------------------------------------------------------------
 //	많은 수의 자료를 처리하기	-	Dog를 읽고 쓴다. read/write
 //------------------------------------------------------------------------------------------------
 #include <iostream>
-#include<string>
+#include<fstream>
+#include<array>
 #include "save.h"
 using namespace std;
 
 class Dog {
-public:
-	Dog() {
-		// 다음 시간에 만들어 보겠습니다.
+private:			// 접근 지시자
+	string name;
+	int id{};
+
+
+	friend ostream& operator<<(ostream& os, const Dog& dog) {			// friend는 우선순위가 최하위?
+		return os << dog.name << " - " << dog.id;
 	}
-private:
-	string name;		// 32
-	size_t id;			// 8
+
+	friend istream& operator>>(ifstream& is, const Dog& dog) {
+		return is.read((char*)&dog, sizeof(Dog));
+	}
 };
 
-//	Dog 만마리를 생성하여 파일에 저장(write). 메모리로 읽어와서 처리(정렬).
+//	[문제] e-classㅔ서 다운 받은 파일 "Dog 10만마리"는 binary 모드로 열었고,
+//	위에 적은 class Dog를 스트림의 write 함수를 사용하여 메모리 그대로 저장하였다.
+
+array<Dog, 10'0000> dogs;
 
 //---------
 int main() 
 //---------
 {
+	ifstream in{ "Dog 10만마리",ios::binary };
+	if (!in)
+		return 20250320;
+
+	//	[문제] 다음 코드가 수정없이 실행되도록 필요한 코드를 class Dog에 추가하라.
+	for (Dog& dog : dogs)
+	{
+		in >> dog;				// operator>>(in,dog)
+		cout << dog << endl;
+	}
+
 	save("main.cpp");
 }
