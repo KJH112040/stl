d8b79bcada1f5af6f31af2cd9bc7a99beb1a9a19
KJH04_ <10wjdgp04@gmail.com>
2025-04-01 15:12:25 +0900

    .

--- 2025stl/2025stl/main.cpp
+++ 2025stl/2025stl/main.cpp
@@ -1,70 +1,59 @@
 //------------------------------------------------------------------------------------------------
-//	2025 STL 화56목78	3월 27일 목요일				(4주 2일)
+//	2025 STL 화56목78	4월 1일 화요일				(5주 1일)
+//	중간고사	(30) -	4월 24일(8주 2일) 목요일
+//	과제		(30) - 4월 10일 설명
 //------------------------------------------------------------------------------------------------
-//	4월 24일 (8주 2일차) - 중간고사
-//	RAII : 자원의 생명주기를 객체의 생명주기에 동기화
-//	오늘의 주제 : Callable
-//	A Callable type is a type for which the INVOKE and INVOKE<R> operations 
-// (used by, e.g., std::function, std::bind, and std::thread::thread) are applicable.
-//	=> Callable type - 호출가능한 타입
-//	1. 
+//	Callable
 //------------------------------------------------------------------------------------------------
 #include <iostream>
-#include<random>
+#include<fstream>
 #include<array>
-#include<print>
-#include<ranges>
 #include<algorithm>
-#include<chrono>
 #include "save.h"
 using namespace std;
 
-//	[문제] 랜덤 int 1000만 개를 메모리에 저장하자.
-//	랜덤 int 값은 [ 0, 1'000'0000 ) ==> 0부터 999'9999까지 값을 갖도록
-//	C++언어의 sort를 사용하여 오름차순으로 정렬하라.
-//	정렬한 결과를 앞에서부터 1000개만 화면에 출력하라.
+//	[설명] Dog 10만 마리를 다음과 같이 파일 "Dog 십만마리"에 저장하였다.
+//	파일은 다음 코드로 열었다.
+//		ofstream out{ "Dog 십만마리" };
+//
+//	Dog 객체는 class Dog의 friend operator<<를 사용하여 저장하였다.
+//
+//	[문제] 이 파일에는 정확하게 10만 개의 Dog 객체가 저장되어 있다.
+//	파일에 저장된 Dog 객체를 모두 읽어 메모리에 저장하라.
+//	제일 마지막 객체의 정보를 화면에 출력하고 답지에도 출력 내용을 적어라.
+//	메모리에 저장된 Dog 객체를 멤버 name 길이 기준 오름차순으로 정렬하라.
+//	정렬된 Dog 객체를 앞에서 부터 1000개를 화면에 출력하라.
 
-array<int, 1'000'0000> a;
+class Dog {
+public:
+	int name_len() {
+		return sizeof(name);
+	}
+private:
+	string name;
+	int id;
+	
+	friend ostream& operator<<(ostream& os, Dog dog) {
+		return os << dog.id << " " << dog.name << " ";
+	}
+};
 
-default_random_engine dre;
-uniform_int_distribution<int> uid{ 0,999'9999 };
-
-bool 오름차순(const int a, const int b) {
-	return a < b;
-}
+array<Dog, 10'0000> dogs;
 
 //---------
 int main() 
 //---------
 {
-	{
-		for (int& num : a)
-			num = uid(dre);
+	ifstream in{ "Dog 십만마리" };
+	in.read((char*)dogs.data(), dogs.size() * sizeof(Dog));
 
-		//	정렬에 걸리는 시간 측정
-		auto 시작 = chrono::high_resolution_clock::now();	//	스톱워치 시작
-		sort<array<int,1'000'0000>::iterator>(a.begin(), a.end(), 오름차순);					//	default 정렬 operator <
-		auto 끝 = chrono::high_resolution_clock::now();		//	스톱워치 끝
+	cout << "마지막 객체의 정보 -" << dogs[9'9999] << endl;
 
-		cout << "경과시간(ms) - " << chrono::duration_cast<chrono::milliseconds>(끝 - 시작) << endl;
-	}
+	sort(dogs.begin(), dogs.end(), [](Dog a, Dog b) {return a.name_len() < b.name_len(); });
 
-	{	// 내림차순으로 정렬
-		for (int& num : a)
-			num = uid(dre);
+	for (int i = 0; i < 1000; ++i)
+		cout << dogs[i] << " ";
+	cout << endl;
 
-		//	정렬에 걸리는 시간 측정
-		auto 시작 = chrono::high_resolution_clock::now();	//	스톱워치 시작
-		sort(a.begin(), a.end(), [](const int a, const int b) {return a > b; });					//	default 정렬 operator <
-		auto 끝 = chrono::high_resolution_clock::now();		//	스톱워치 끝
-
-		cout << "경과시간(ms) - " << chrono::duration_cast<chrono::milliseconds>(끝 - 시작) << endl;
-	}
-
-	//	앞에서 부터 1000개를 화면 출력
-	//for (int num:a	| ranges::views::take(1000))
-	//	print("{:8}", num);
-	//cout << endl;
-
-	save("main.cpp");
+	//save("main.cpp");
 }
