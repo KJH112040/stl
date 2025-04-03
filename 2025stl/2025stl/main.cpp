028ead401618e4c0383efd261b3d589c7aa7a44b
KJH04_ <10wjdgp04@gmail.com>
2025-04-03 17:11:36 +0900

    .

--- 2025stl/2025stl/main.cpp
+++ 2025stl/2025stl/main.cpp
@@ -1,59 +1,51 @@
 //------------------------------------------------------------------------------------------------
-//	2025 STL 화56목78	4월 1일 화요일				(5주 1일)
+//	2025 STL 화56목78	4월 3일 목요일				(5주 2일)
 //	중간고사	(30) -	4월 24일(8주 2일) 목요일
 //	과제		(30) - 4월 10일 설명
 //------------------------------------------------------------------------------------------------
-//	Callable
+//	STRING - std::string과 유사한 클래스, STL 표준 컨테이너가 되도록..
 //------------------------------------------------------------------------------------------------
 #include <iostream>
-#include<fstream>
-#include<array>
-#include<algorithm>
+#include<string>
 #include "save.h"
 using namespace std;
 
-//	[설명] Dog 10만 마리를 다음과 같이 파일 "Dog 십만마리"에 저장하였다.
-//	파일은 다음 코드로 열었다.
-//		ofstream out{ "Dog 십만마리" };
-//
-//	Dog 객체는 class Dog의 friend operator<<를 사용하여 저장하였다.
-//
-//	[문제] 이 파일에는 정확하게 10만 개의 Dog 객체가 저장되어 있다.
-//	파일에 저장된 Dog 객체를 모두 읽어 메모리에 저장하라.
-//	제일 마지막 객체의 정보를 화면에 출력하고 답지에도 출력 내용을 적어라.
-//	메모리에 저장된 Dog 객체를 멤버 name 길이 기준 오름차순으로 정렬하라.
-//	정렬된 Dog 객체를 앞에서 부터 1000개를 화면에 출력하라.
-
-class Dog {
+// public, private, protected ---> acssess modifiy모디파이?
+class STRING {
 public:
-	int name_len() {
-		return sizeof(name);
+	STRING(const char* s) :len{strlen(s)} {		// [] (array) -> *(pointer)로 collapsing 
+		// p = new char[len];	-> unique_ptr이기에 불가능한 작업, unique_ptr은 자원을 독점하는 놈
+		p.reset();
+		p = make_unique<char[]>(len);		// Lvalue : 현대식으로는 location value (left value아님...)
+		memcpy(p.get(), s, len);					// DMA : Direct Memory Acssess
+	};
+
+	size_t size() const {
+		return len;
 	}
 private:
-	string name;
-	int id;
-	
-	friend ostream& operator<<(ostream& os, Dog dog) {
-		return os << dog.id << " " << dog.name << " ";
+	size_t len{};
+	unique_ptr<char[]> p{};
+
+	friend ostream& operator<<(ostream & os, const STRING& str) {
+		for (int i = 0; i < str.len; ++i)
+			os << str.p[i];
+		return os;
 	}
 };
-
-array<Dog, 10'0000> dogs;
 
 //---------
 int main() 
 //---------
 {
-	ifstream in{ "Dog 십만마리" };
-	in.read((char*)dogs.data(), dogs.size() * sizeof(Dog));
+	STRING s{ "std::string과 유사한 클래스" };
 
-	cout << "마지막 객체의 정보 -" << dogs[9'9999] << endl;
+	cout << s.size() << endl;		// s가 확보한 자원의 바이트 수
 
-	sort(dogs.begin(), dogs.end(), [](Dog a, Dog b) {return a.name_len() < b.name_len(); });
+	cout << s << endl;
 
-	for (int i = 0; i < 1000; ++i)
-		cout << dogs[i] << " ";
-	cout << endl;
+	//	27
+	//	std::string과 유사한 클래스
 
-	//save("main.cpp");
+	save("main.cpp");
 }
