c3847a0a19383032fdb50246d75793392c69208f
KJH04_ <10wjdgp04@gmail.com>
2025-04-29 15:10:44 +0900

    .

--- 2025stl/2025stl/main.cpp
+++ 2025stl/2025stl/main.cpp
@@ -1,16 +1,18 @@
 //------------------------------------------------------------------------------------------------
-//	2025 STL 화56목78				4월 22일 화요일					(8주 1일)
-//	중간고사	(30) -	4월 24일(8주 2일) 목요일	-	시험장소 E동 320 (준비 끝난 후 60분)
-//	5월 2일(화요일) -> 6월 19일(예정)
+//	2025 STL 화56목78				4월 29일 화요일					(9주 1일)
+//	5월 6일(화요일) -> 6월 19일(예정)
 //------------------------------------------------------------------------------------------------
 //	STL Container - Container are objects that store other objects.
 //	1. Sequence Containers
 //		array<T,N>
 //		vector<T> - dynamic(run-time) array
-//		deque<T> - vector보다 더 많은 원소를 담을 수 있다.
+//		deque<T>  - vector보다 더 많은 원소를 담을 수 있다.
+//		
 //------------------------------------------------------------------------------------------------
 #include <iostream>
-#include <deque>
+#include <vector>
+#include <list>
+#include<algorithm>
 #include"STRING.h"
 #include "save.h"
 using namespace std;
@@ -22,13 +24,14 @@ int main()
 //---------
 {
 	관찰 = true;
-	deque<STRING> d{"1","22","333","4444","55555"};
+	list<STRING> cont{ "1","333","55555","4444","22" };
 
-	for (const STRING& s : d)
-		cout << (void*)&s << endl;
+	ranges::sort(cont.begin(), cont.end());
+
+	for (const STRING& s : cont)
+		cout << s << endl;
+
+	
 	관찰 = false;
-
-
-
 	save("main.cpp");
 }
