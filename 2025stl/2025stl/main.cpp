df6963037536cef24e605c811eb41c013b85d107
KJH04_ <10wjdgp04@gmail.com>
2025-05-29 17:12:41 +0900

    .

--- 2025stl/2025stl/main.cpp
+++ 2025stl/2025stl/main.cpp
@@ -1,5 +1,5 @@
 //------------------------------------------------------------------------------------------------
-//	2025 STL 화56목78				5월 27일 화요일					(12주 2일)
+//	2025 STL 화56목78				5월 29일 목요일					(13주 1일)
 //	6월 19일 목요일 15주 2일 - 기말시험
 //------------------------------------------------------------------------------------------------
 //	Associative Container	- key와 연관된 value를 항상 정렬상태로 유지한다.
@@ -9,10 +9,10 @@
 //------------------------------------------------------------------------------------------------
 #include <iostream>
 #include<fstream>
-#include<vector>
 #include<algorithm>
-#include<set>
-#include<ranges>
+#include<map>
+#include<print>
+#include<format>
 #include"STRING.h"
 #include "save.h"
 using namespace std;
@@ -20,8 +20,10 @@ using namespace std;
 extern bool 관찰;				// 관찰하려면 true로 설정
 
 // 강의 자료 "이상한 나라의 앨리스.txt"를 다운
-// [1] 여기에 있는 모든 단어를 multiset<STRING>에 저장하라.
+// [1] 여기에 있는 모든 단어를 multimap<STRING,size_t>에 저장하라.
 // [2] 모두 몇 단어인지 화면에 출력하라.
+// [3] 단어와 사용 횟수를 화면에 출력하라.
+
 
 //---------
 int main()
@@ -31,34 +33,18 @@ int main()
 	if (!in)
 	{
 		cout << "파일?" << endl;
-		return 20250527;
+		return 20250529;
 	}
 
-	multiset<STRING> ms{ istream_iterator<STRING>{in},{} };
-	cout << "읽은 단어 수 - " << ms.size() << endl;
-	// 읽은 단어 수 - 26626
+	map<STRING, size_t> wordNum;
+
+	STRING 단어;
+	while (in >> 단어)
+		wordNum[단어]++;
+	
+	for (auto [단어, 개수] : wordNum)		// 단어가 STRING이라 formatting이 안 됨.
+		cout << 단어 << " - " << 개수 << endl;
+		//println("{:20} - {:}", (char*)(단어[0]), 개수);
 
 	save("main.cpp");
-
-	for (const STRING& 단어 : ms) 
-		cout << 단어 << " ";
-	cout << endl;
-
-	// 동일 단어가 몇 개 있는지 알려주자
-	while (true) {
-		cout << "찾을 단어? ";
-		STRING 단어;
-		cin >> 단어;
-
-		cout << ms.count(단어) << "개가 있습니다." << endl;
-	}
-	//	정렬된 상태
-	//	you가 들어갈 수 있는 하한선과 상한선을 찾음
-	//  you의 범위가 나옴. 그 부분 개수를 count
-	// 
-	//	찾을 단어 ? a
-	//	619개가 있습니다.
-	//	찾을 단어 ? the
-	//	1529개가 있습니다.
-	//	찾을 단어 ?
 }
